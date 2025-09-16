ac7fdddc18bf54efe675539aacfbc46055cb01b2
PRIYA DHARSHINI D <priyadharshiniofficial2024@gmail.com>
2025-09-16 21:34:27 +0530

    Update 49. Group Anagrams.py

--- String/49. Group Anagrams.py
+++ String/49. Group Anagrams.py
@@ -13,3 +13,20 @@ class Solution:
         return list(strs_table.values())
 
 #62.68% Beats
+
+class Solution: #82.60%
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagrams = {}
+        
+        for s in strs:
+            s_sorted = "".join(sorted(s))
+            if s_sorted not in anagrams:
+                anagrams[s_sorted] = []
+            anagrams[s_sorted].append(s)
+            
+        return [list(value) for key, value in anagrams.items()]
+
+"""
+Time: O(n · k log k)
+Space: O(n · k)
+"""
